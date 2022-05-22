053086caaae13fcd67c183291b32b9d8cbf85df1
aleballabio <96348218+aleballabio@users.noreply.github.com>
2022-05-22 17:57:27 +0200

    fixes

--- database/migrations/2022_05_18_161041_create_posts_table.php
+++ database/migrations/2022_05_18_161041_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id')->unique();
+            $table->unsignedBigInteger('user_id');
             $table->string('title', 100);
             $table->text('content');
             $table->string('slug', 100)->unique();
